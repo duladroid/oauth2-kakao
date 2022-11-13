d4cd91eee8c5c3e21f71c138db73fef36b07ade2
yhwang15 <donkassu@gmail.com>
2022-11-13 23:08:08 +0900

    gson import 성공

--- src/main/java/yhwang15/kakaologin/KakaoApi.java
+++ src/main/java/yhwang15/kakaologin/KakaoApi.java
@@ -1,8 +1,15 @@
 package yhwang15.kakaologin;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class KakaoApi {
     public String getAccessToken(String code) {
@@ -18,6 +25,29 @@ public class KakaoApi {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode " + responseCode);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line = "";
+            String result = "";
+
+            while((line = bufferedReader.readLine()) != null){
+                result += line;
+            }
+            System.out.println("response body = " + result);
+
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+            JsonObject kakaoAcount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+            String email = properties.getAsJsonObject().get("email").getAsString();
+
 
         } catch (Exception e){
 
