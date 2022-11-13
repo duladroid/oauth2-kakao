d3f1ffb91c808956894d9e3ae6eacadcef380264
yhwang15 <donkassu@gmail.com>
2022-11-13 23:31:00 +0900

    Kakao Api 완료

--- src/main/java/yhwang15/kakaologin/KakaoApi.java
+++ src/main/java/yhwang15/kakaologin/KakaoApi.java
@@ -2,18 +2,66 @@ package yhwang15.kakaologin;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class KakaoApi {
     public String getAccessToken(String code) {
-        return null;
+        String accessToken = "";
+        String refreshToken = "";
+        String reqURL = "https://kapi.kakao.com/oauth/token";
+
+        try{
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("grant_type = authorization_code");
+            stringBuilder.append("&client_id=50229b49e1874f7ccd32a3470eab23a3");
+            stringBuilder.append("&redirect_uri=http://localhost:8000/login");
+            stringBuilder.append("&code="+code);
+
+            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.flush();
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("response code = " + responseCode);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line = "";
+            String result = "";
+            while((line = bufferedReader.readLine())!= null){
+                result += line;
+            }
+            System.out.println("response body = " + result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            accessToken = element.getAsJsonObject().get("access_Token").getAsString();
+            refreshToken = element.getAsJsonObject().get("refresh_Token").getAsString();
+
+            bufferedReader.close();
+            bufferedWriter.close();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+        return accessToken;
     }
 
     public HashMap<String, Object> getUserInfo(String accessToken) {
@@ -46,13 +94,17 @@ public class KakaoApi {
             JsonObject kakaoAcount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
 
             String nickname = properties.getAsJsonObject().get("nickname").getAsString();
-            String email = properties.getAsJsonObject().get("email").getAsString();
+            String email = kakaoAcount.getAsJsonObject().get("email").getAsString();
 
+            userInfo.put("nickname", nickname);
+            userInfo.put("email", email);
 
         } catch (Exception e){
-
+            e.printStackTrace();
         }
+        return userInfo;
 
-        return null;
     }
+
+
 }
