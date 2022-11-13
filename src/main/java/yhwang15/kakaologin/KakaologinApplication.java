bd3794dfb39c3c59d000262210b10ea778feb2e1
yhwang15 <donkassu@gmail.com>
2022-11-13 21:43:34 +0900

    init. spring settings

---
+++ src/main/java/yhwang15/kakaologin/KakaologinApplication.java
@@ -0,0 +1,13 @@
+package yhwang15.kakaologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaologinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaologinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yhwang15/kakaologin/KakaologinApplicationTests.java
@@ -0,0 +1,13 @@
+package yhwang15.kakaologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaologinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
